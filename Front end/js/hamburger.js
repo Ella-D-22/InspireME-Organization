990a09f73840c9adf417edc1cc548ab0ddb426be
Ella-D-22 <muringoviolet@gmail.com>
2024-12-09 22:13:37 +0300

    Overwriting repository content with new subfolder

--- Front end/js/hamburger.js
+++
@@ -1,4 +0,0 @@
-function toggleMenu() {
-    const nav = document.querySelector("header nav");
-    nav.classList.toggle("active");
-}
--- Front end/js/modal.js
+++
@@ -1,9 +0,0 @@
-// Function to open the video modal
-function openVideoModal() {
-    document.getElementById('videoModal').style.display = "block";
-}
-
-// Function to close the video modal
-function closeVideoModal() {
-    document.getElementById('videoModal').style.display = "none";
-}
