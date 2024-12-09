fceb34fb1ae8ab99281aa325715ca5487186d22c
Ella-D-22 <muringoviolet@gmail.com>
2024-12-09 14:33:32 +0300

    Initial commit - InspireME website

---
+++ js/modal.js
@@ -0,0 +1,9 @@
+// Function to open the video modal
+function openVideoModal() {
+    document.getElementById('videoModal').style.display = "block";
+}
+
+// Function to close the video modal
+function closeVideoModal() {
+    document.getElementById('videoModal').style.display = "none";
+}
